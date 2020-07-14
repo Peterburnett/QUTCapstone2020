0ef5e39bbd6b42f6f95b7980f4b7035cfafcf99e
harunaka <51142777+harunaka11037@users.noreply.github.com>
2020-07-14 12:57:05 +1000

    Fixed Travis errors

--- settings.php
+++ settings.php
@@ -44,13 +44,13 @@ if ($hassiteconfig) {
 
 
     // Create settings
-    $heading = new admin_setting_heading('tool_paymentplugin_gsettings/heading', $globalsettings->visiblename, 
+    $heading = new admin_setting_heading('tool_paymentplugin_gsettings/heading', $globalsettings->visiblename,
         get_string('gsettingsdesc', 'tool_paymentplugin'));
 
-    $disableallcheck = new admin_setting_configcheckbox('tool_paymentplugin_gsettings/disablePurchases', get_string('gsettingsdisablepurchase', 'tool_paymentplugin'), 
+    $disableallcheck = new admin_setting_configcheckbox('tool_paymentplugin_gsettings/disablePurchases', get_string('gsettingsdisablepurchase', 'tool_paymentplugin'),
         get_string('gsettingsdisablepurchasedesc', 'tool_paymentplugin'), 0);
 
-    $checkbox2 = new admin_setting_configcheckbox('tool_paymentplugin_gsettings/checkbox2', get_string('gsettingscheck2', 'tool_paymentplugin'), 
+    $checkbox2 = new admin_setting_configcheckbox('tool_paymentplugin_gsettings/checkbox2', get_string('gsettingscheck2', 'tool_paymentplugin'),
         get_string('gsettingscheck2desc', 'tool_paymentplugin'), 0);
 
     $exampleselections = [
@@ -58,29 +58,32 @@ if ($hassiteconfig) {
             'Option B' => get_string('gsettingsmulti1selectionB', 'tool_paymentplugin'),
             'Option C' => get_string('gsettingsmulti1selectionC', 'tool_paymentplugin')
         ];
-    $multiselect = new admin_setting_configmultiselect('tool_paymentplugin_gsettings/multi1', get_string('gsettingsmulti1', 'tool_paymentplugin'), 
+    $multiselect = new admin_setting_configmultiselect('tool_paymentplugin_gsettings/multi1', get_string('gsettingsmulti1', 'tool_paymentplugin'),
         get_string('gsettingsmulti1desc', 'tool_paymentplugin'), [], $exampleselections);
 
     $entryfield = new admin_setting_configtext('tool_paymentplugin_test_entryfield', 'Entry Field:', 'The box below will hold this value', '');
-    if ($entryfield->get_setting() == '') $entryfield->write_setting("For Example...");
+    if ($entryfield->get_setting() == '') {
+        $entryfield->write_setting("For Example...");
+    }
 
     $resultbox = new admin_setting_configtextarea('tool_paymentplugin_test_result', 'Result: ', '', '');
     // $resultbox->nosave = false; // Dont save settings results
-    $resultbox->write_setting('Got result from above field. "'.$entryfield->get_setting().'" | "'.get_config('tool_paymentplugin', 'tool_paymentplugin_test_entryfield').'" | "'.$CFG->tool_paymentplugin_test_entryfield.'"');
-    //echo ;
-    $textboxnumbersonly = new admin_setting_configtext_with_maxlength('tool_paymentplugin_gsettings/text1', get_string('gsettingstext1', 'tool_paymentplugin'), 
+    $resultbox->write_setting('Got result from above field. "'.$entryfield->get_setting()
+    .'" | "'.get_config('tool_paymentplugin', 'tool_paymentplugin_test_entryfield').'" | "'.$CFG->tool_paymentplugin_test_entryfield.'"');
+    // echo ;
+    $textboxnumbersonly = new admin_setting_configtext_with_maxlength('tool_paymentplugin_gsettings/text1', get_string('gsettingstext1', 'tool_paymentplugin'),
         get_string('gsettingstext1desc', 'tool_paymentplugin'), '', PARAM_INT, 1, 3);
     
-    $textboxtextonly = new admin_setting_configtext_with_maxlength('tool_paymentplugin_gsettings', get_string('gsettingstext2', 'tool_paymentplugin'), 
+    $textboxtextonly = new admin_setting_configtext_with_maxlength('tool_paymentplugin_gsettings', get_string('gsettingstext2', 'tool_paymentplugin'),
         get_string('gsettingstext2desc', 'tool_paymentplugin'), '', PARAM_TEXT, 10, 20);
 
-    $textboxemailonly = new admin_setting_configtext_with_maxlength('tool_paymentplugin_gsettings/text3', get_string('gsettingstext3', 'tool_paymentplugin'), 
+    $textboxemailonly = new admin_setting_configtext_with_maxlength('tool_paymentplugin_gsettings/text3', get_string('gsettingstext3', 'tool_paymentplugin'),
         get_string('gsettingstext3desc', 'tool_paymentplugin'), '', PARAM_EMAIL, 0, 70);
-    
+
 
     // Add settings
     $globalsettings->add($heading);
-    $globalsettings->add($disableallcheck);   
+    $globalsettings->add($disableallcheck);
     $globalsettings->add($checkbox2);
     $globalsettings->add($multiselect);
     $globalsettings->add($entryfield);
