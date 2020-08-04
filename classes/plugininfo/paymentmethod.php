f601dd6224284e85140a1ca07101248bf7bc24a4
harunaka <51142777+harunaka11037@users.noreply.github.com>
2020-08-04 13:29:11 +1000

    Fixed paypal plugininfo namespace

--- classes/plugininfo/paymentmethod.php
+++ classes/plugininfo/paymentmethod.php
@@ -23,7 +23,7 @@
  * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-namespace tool\paymentplugin;
+namespace tool_paymentplugin\plugininfo;
 
 defined('MOODLE_INTERNAL') || die();
 
