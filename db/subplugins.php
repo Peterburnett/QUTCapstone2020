1ae6919282e713bb9058d313305af2b09db9f0e1
harunaka <51142777+harunaka11037@users.noreply.github.com>
2020-08-06 01:02:56 +1000

    Fixed subplugins.php description

--- db/subplugins.php
+++ db/subplugins.php
@@ -15,7 +15,7 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
- * The info class for paymentgateway subplugin type.
+ * Definition of paymentplugin subplugins.
  *
  * @package   tool_paymentplugin
  * @copyright 2020 MAHQ
