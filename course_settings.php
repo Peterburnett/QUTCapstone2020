542389777947db62acf70c809cba5f35ded0d908
Mitch1404 <mhalp1406@gmail.com>
2020-08-10 16:40:47 +1000

    Travis Fix

--- course_settings.php
+++ course_settings.php
@@ -27,59 +27,55 @@
  * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  **/
 
-        require_once(__DIR__.'/../../../config.php');
-        require_once('payment_settings_form.php');
+require_once(__DIR__.'/../../../config.php');
+require_once('payment_settings_form.php');
 
-        // Login check
-        $courseid = optional_param('id', 0, PARAM_INT);
+// Login check
+$courseid = optional_param('id', 0, PARAM_INT);
 
-        if (empty($courseid))  {
-                throw new moodle_exception('No valid course id detected.');
+if (empty($courseid)) {
+        throw new moodle_exception('No valid course id detected.');
+}
+
+$course = get_course($courseid);
+require_login($courseid, true);
+$coursecontext = context_course::instance(course_get_format($course)->get_course()->id);
+require_capability('moodle/course:create', $coursecontext);
+
+// Setup Page
+$title = get_string('coursesettings_management:title', 'tool_paymentplugin');
+$PAGE->set_url('/admin/tool/paymentplugin/course_settings.php');
+$PAGE->set_pagelayout('admin'); // What this do?
+$PAGE->set_context(context_course::instance($courseid));
+$PAGE->set_cacheable(false); // What this do?
+
+$PAGE->set_heading($title);
+$PAGE->navbar->add($title, new moodle_url('/admin/tool/paymentplugin/course_settings.php'));
+
+// Display Page
+echo $OUTPUT->header();
+
+// The settings
+$args = array(
+        'course' => $course,
+        'id' => $courseid,
+        );
+$paymentform = new payment_settings_form(new moodle_url('/admin/tool/paymentplugin/course_settings.php', array('id' => $courseid)), $args);
+
+if (($formdata = $paymentform->get_data()) && !($paymentform->is_cancelled())) {
+        $tablename = 'tool_paymentplugin_course';
+        $cost = $formdata->coursecost;
+        
+        if ($DB->record_exists($tablename, ['courseid' => $courseid])) {
+                $record = $DB->get_record($tablename, ['courseid' => $courseid]);
+                $record->cost = $cost;
+                $DB->update_record($tablename, $record);
+        } else {
+                $record = (object) array('courseid' => $courseid, 'cost' => $cost);
+                $DB->insert_record($tablename, $record);
         }
+} 
+// Display the form
+$paymentform->display();
 
-        $course = get_course($courseid);
-        require_login($courseid, true);
-        $coursecontext = context_course::instance(course_get_format($course)->get_course()->id);
-        require_capability('moodle/course:create', $coursecontext);
-
-        // Setup Page
-        $title = get_string('coursesettings_management:title', 'tool_paymentplugin');
-        $PAGE->set_url('/admin/tool/paymentplugin/course_settings.php');
-        $PAGE->set_pagelayout('admin'); // What this do?
-        $PAGE->set_context(context_course::instance($courseid));
-        $PAGE->set_cacheable(false); // What this do?
-
-        $PAGE->set_heading($title);
-        $PAGE->navbar->add($title, new moodle_url('/admin/tool/paymentplugin/course_settings.php'));
-
-        // Display Page
-        echo $OUTPUT->header();
-
-        // The settings
-        $args = array(
-                'course' => $course,
-                'id' => $courseid,
-                );
-        $paymentform = new payment_settings_form(new moodle_url('/admin/tool/paymentplugin/course_settings.php', array('id'=>$courseid)), $args);
-
-        if ($paymentform->is_cancelled())       {
-                // Cancel
-        }
-        else if ($formdata = $paymentform->get_data())      {
-                $tablename = 'tool_paymentplugin_course';
-                $cost = $formdata->coursecost;
-                
-                if ($DB->record_exists($tablename, ['courseid' => $courseid]))   {
-                        $record = $DB->get_record($tablename, ['courseid' => $courseid]);
-                        $record->cost = $cost;
-                        $DB->update_record($tablename, $record);
-                }
-                else    {
-                        $record = (object) array('courseid' => $courseid, 'cost' => $cost);
-                        $DB->insert_record($tablename, $record);
-                }
-        } 
-        // Display the form
-        $paymentform->display();
-
-        echo $OUTPUT->footer();
+echo $OUTPUT->footer();
