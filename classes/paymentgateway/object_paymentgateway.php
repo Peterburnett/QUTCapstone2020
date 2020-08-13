9639d2f38aa07e3e158a0c9a3920e49174f871ac
Mitch1404 <mhalp1406@gmail.com>
2020-08-13 14:46:06 +1000

    Added Comments + x2 Function Placeholders

--- classes/paymentgateway/object_paymentgateway.php
+++ classes/paymentgateway/object_paymentgateway.php
@@ -1,4 +1,30 @@
 <?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Abstract class for all payment gateway objects. 
+ *
+ * File         object_paymentgateway.php
+ * Encoding     UTF-8
+ *
+ * @package     tool_paymentplugin\classes\paymentgateway
+ *
+ * @copyright   MAHQ
+ * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ **/
 
 namespace tool_paymentplugin\classes\paymentgateway;
 
@@ -7,7 +33,16 @@ defined ('MOODLE_INTERNAL') || die();
 abstract class object_paymentgateway {
     public $name;
 
-    public function __construct($name)    {
+    public function __construct($name) {
         $this->name = $name.'test';
+    }
+
+    public function get_display_name() {
+        return get_string('pluginname');
+    }
+
+    // TO DO
+    public function is_active() {
+        return true;
     }
 }
