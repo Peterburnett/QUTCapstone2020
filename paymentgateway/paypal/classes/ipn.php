c6ec8b8258c619dfe5c305ad23a8538d8725a354
harunaka <51142777+harunaka11037@users.noreply.github.com>
2020-10-15 17:01:16 +1000

    Fixed validationrequest

--- paymentgateway/paypal/classes/ipn.php
+++ paymentgateway/paypal/classes/ipn.php
@@ -43,9 +43,9 @@ class ipn {
     private $validationrequest;
 
     private function create_validation_request() {
-        $validationrequest = 'cmd=_notify-validate';
+        $this->validationrequest = 'cmd=_notify-validate';
         foreach ($_POST as $key => $value) {
-            $validationrequest .= "&$key=".urlencode($value);
+            $this->validationrequest .= "&$key=".urlencode($value);
         }
     }
 
